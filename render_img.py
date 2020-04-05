24bb2c28cae589596c33bc5ec06abd9af567f559
Ash Rust <ash@ashrust.com>
2020-04-05 03:38:34 +0000

    Adding JS to move forms on keyup.

--- render_img.py
+++ render_img.py
@@ -36,20 +36,52 @@ html {
   font: 16px arial,sans-serif ;
   color: #333;
 }
+#ddg {
+  float:left; padding-right: 10px;
+}
+#goog{
+  float:left;
+}
+.entered {
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  display: none;
+}
+
+.largetext {
+  font-size: 24px;
+}
+
+input[type=text]:focus {
+  background-color: lightblue;
+}
 </style>
+<script>
+var inputBoxes = document.getElementsByClassName('input');
+for (var i = 0; i < inputBoxes.length; i++) {
+  inputBoxes[x].onkeyup = function() {
+    parent_div = inputBoxes[x].parentElement.parentElement;
+    parent_div.classList.add("entered");
+    parent_div.removeAttribute("id");
+    inputBoxes[x].classList.add("largetext");
+  }
+}
+
+</script>
 </head>
 <body>
 <div class='bgimg-1'>
 <div class="bottomright"> IMGTXT </div>
 <div class="bottomleft">
-  <div style="float:left; padding-right: 10px;">
+  <div id="ddg">
   <form id="ddgSearch" method="get" action="https://www.duckduckgo.com/search" style="padding:0; margin:0;">
-    <input id="ddgSearchBar" type="text" name="q" size="25" value="" autofocus="autofocus" style="font-size:16px;" placeholder="DuckDuckGo Search">
+    <input id="ddgSearchBar" class="input" type="text" name="q" size="25" value="" autofocus="autofocus" style="font-size:16px;" placeholder="DuckDuckGo Search">
   </form>
   </div>
-  <div style="float:left;">
-  <form id="googleSearch" method="get" action="https://www.google.com/search" style="padding:0; margin:0;">
-    <input id="googleSearchBar" type="text" name="q" size="25" value="" style="font-size:16px;" placeholder="Google Search">
+  <div id="goog" class="input" >
+  <form id="googleSearch"  method="get" action="https://www.google.com/search" style="padding:0; margin:0;">
+    <input id="googleSearchBar" class="input" type="text" name="q" size="25" value="" style="font-size:16px;" placeholder="Google Search">
   </form>
   </div>
 </div>

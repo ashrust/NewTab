a9e03eeecce22538ef15c4112f13bc33b30a41a7
Ash Rust <ash@ashrust.com>
2020-04-07 03:33:56 +0000

    added dog result collection, need to render it now

--- autocomplete.py
+++ autocomplete.py
@@ -1,23 +1,32 @@
 import json, requests
 
+#suggestion urls
 goog_url = "http://suggestqueries.google.com/complete/search?client=safari&q="
+ddg_url ="https://duckduckgo.com/ac/?kl=wt-wt&q="
 
-def google_suggestions(query):
-  results = get_google_results(query)
-  results = json.loads(results)
+def get_suggestions(engine, query):
+  response = get_results(engine, query)
+  results = json.loads(response)
   final = []
-  #print("results", results[1][1][0])
-  for r in results[1]:
-    final.append(r[0].strip())
-  print ("ac final", final)
+
+  if engine == 'google':
+    for r in results[1]:
+      final.append(r[0].strip())
+  else:
+    for r in results:
+      final.append(r['phrase'])
+
   return final
-
-
-def get_google_results(query):
+  
+#hit google server for suggestions and return response
+def get_results(engine, query):
   #print ("url parts",goog_url, query)
-  send_url = goog_url+query
+  send_url = ""
+  if engine == "google": send_url = goog_url+query
+  else: send_url = ddg_url+query
   #print ("url final",send_url)
   resp = requests.get(url=send_url)
   #print ("response text", resp.text)
   return resp.text
+
 

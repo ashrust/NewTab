55054fc420bb2a7957f426e82808d44141d90b7b
Ash Rust <ash@ashrust.com>
2020-04-09 03:02:02 +0000

    logging off

--- autocomplete.py
+++ autocomplete.py
@@ -5,7 +5,7 @@ goog_url = "http://suggestqueries.google.com/complete/search?client=safari&q="
 ddg_url ="https://duckduckgo.com/ac/?kl=wt-wt&q="
 
 def get_suggestions(engine, query):
-  print ("eng, q", engine, query)
+  #print ("eng, q", engine, query)
   response = get_results(engine, query)
   results = json.loads(response)
   final = []
@@ -21,11 +21,11 @@ def get_suggestions(engine, query):
   
 #hit google server for suggestions and return response
 def get_results(engine, query):
-  print ("url parts",goog_url, query)
+  #print ("url parts",goog_url, query)
   send_url = ""
   if engine == "google": send_url = goog_url+query
   else: send_url = ddg_url+query
-  print ("url final",send_url)
+  #print ("url final",send_url)
   resp = requests.get(url=send_url)
   #print ("response text", resp.text)
   return resp.text

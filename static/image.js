1d08bd5681608967f10aa7c4eebf2592d59550b0
Ash Rust <ash@ashrust.com>
2020-04-19 20:08:06 +0000

    Background img load bugfix

--- static/image.js
+++ static/image.js
@@ -1,7 +1,7 @@
 function showBackground(){
-  /*console.log('showing background');*/
+  console.log('showing background');
   elem = document.getElementById("bg");
   elem.style.opacity='1';
   elem.style.marginTop ='0px';
 }
-document.addEventListener("load", showBackground);
+window.addEventListener("load", showBackground);
